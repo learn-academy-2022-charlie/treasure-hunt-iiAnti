9a5aa9c0dffd31bfe12db9dec47c452f8d22aae3
ios10 <trikshotzz05@gmail.com>
2022-05-31 13:28:41 -0700

    winning square

--- src/App.js
+++ src/App.js
@@ -6,14 +6,35 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasureLocation: null,
+      bombLocation: null
     }
   }
 
+  componentDidMount() {
+    let treasure = Math.floor(Math.random() * 
+    this.state.board.length)
+    let bomb = Math.floor(Math.random() * 
+    this.state.board.length)
+    this.setState({
+      treasureLocation: treasure, 
+      bombLocation: bomb
+    })
+  }
+
   handleGamePlay = (index) => {
-   const {board} = this.state
-   board[index] = "🌳"
-   this.setState({board: board})
+    const {board, treasureLocation, bombLocation} = this.state
+    if( index === treasureLocation){
+      board[index] ="💎"
+      this.setState({board: board})
+    }else if(index === bombLocation){
+      board[index] = "💣"
+      this.setState({board: board})
+    } else{
+      board[index] = "🌳"
+      this.setState({board: board})
+   }
   }
 
   render() {
